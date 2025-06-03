bf1909967d576fd93304f3512c011732558c3cb2
Enkoma <Khodzhgorovaenkira@yandex.ru>
2025-06-03 15:50:26 +0300

    два цикла

--- 3a4et/iter_flatteener.py
+++ 3a4et/iter_flatteener.py
@@ -1,15 +1,20 @@
 class FlattenIterator:
     def __init__(self, nested_list):
-        self.stack = nested_list[::-1]
+        self.nested_list = nested_list
+        self.reset()
+
+    def reset(self):
+        self.stack = list(reversed(self.nested_list))
 
     def __iter__(self):
+        self.reset()
         return self
 
     def __next__(self):
         while self.stack:
-            top = self.stack.pop()
-            if isinstance(top, list):
-                self.stack.extend(top[::-1])
+            current = self.stack.pop()
+            if isinstance(current, list):
+                self.stack.extend(reversed(current))
             else:
-                return top
+                return current
         raise StopIteration
