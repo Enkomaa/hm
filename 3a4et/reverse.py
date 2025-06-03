05e6b5d26184a7568c898999acb00e54c125e726
Enkoma <Khodzhgorovaenkira@yandex.ru>
2025-06-03 15:40:16 +0300

    исправленные однобуквенные переменные

--- 3a4et/reverse.py
+++ 3a4et/reverse.py
@@ -1,16 +1,19 @@
-def reverseStr(s, k):
-    result = []
-    string_length = len(s)
+def reverseStr(input_string, group_size):
+    result_parts = []
+    string_length = len(input_string)
 
-    for start_index in range(0, string_length, 2 * k):
-        block = s[start_index:start_index + 2 * k]
+    for block_start in range(0, string_length, 2 * group_size):
+        current_block = input_string[block_start:block_start + 2 * group_size]
 
-        if len(block) < k:
-            result.append(block[::-1])
+        if len(current_block) < group_size:
+            result_parts.append(current_block[::-1])
         else:
-            result.append(block[:k][::-1] + block[k:])
+            reversed_part = current_block[:group_size][::-1]
+            remaining_part = current_block[group_size:]
+            result_parts.append(reversed_part + remaining_part)
 
-    return "".join(result)
+    return "".join(result_parts)
+
 
 print(reverseStr("abcdefg", 2))
 print(reverseStr("abcd", 2))
